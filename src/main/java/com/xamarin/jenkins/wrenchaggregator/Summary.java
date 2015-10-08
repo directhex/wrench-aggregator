48f763928cec51535b1b032126517021179c159a
Jo Shields <jo.shields@xamarin.com>
2015-10-08 12:31:14 +0100

    First hash at reading Groovy Postbuild <table> & feeding it back

--- src/main/java/com/xamarin/jenkins/wrenchaggregator/Summary.java
+++ src/main/java/com/xamarin/jenkins/wrenchaggregator/Summary.java
@@ -2,7 +2,6 @@ package com.xamarin.jenkins.wrenchaggregator;
 
 import hudson.Extension;
 import hudson.matrix.MatrixConfiguration;
-import hudson.scm.*;
 import hudson.plugins.git.*;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
@@ -11,8 +10,18 @@ import hudson.model.InvisibleAction;
 import hudson.model.Result;
 import hudson.model.TransientProjectActionFactory;
 import hudson.util.RunList;
+import java.io.StringReader;
 import java.util.Arrays;
 import java.util.Collection;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import org.jvnet.hudson.plugins.groovypostbuild.GroovyPostbuildSummaryAction;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 public class Summary extends InvisibleAction {
 
@@ -34,6 +43,23 @@ public class Summary extends InvisibleAction {
     
     public String getRepoUrl() {
         return ((GitSCM)(project.getScm())).getBrowser().getRepoUrl();
+    }
+    
+    public String getSummary(AbstractBuild<?, ?> target) {
+        String rawStatus = ((GroovyPostbuildSummaryAction)(target.getActions(GroovyPostbuildSummaryAction.class).toArray()[0])).getText();
+        rawStatus = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + rawStatus.substring(rawStatus.indexOf("</h1>") + 5);
+        try {
+            StringBuilder result = new StringBuilder();
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            InputSource inputSource = new InputSource( new StringReader( rawStatus ) );
+            NodeList nodes = (NodeList) xpath.evaluate("/table/tr/td[position()=4]", inputSource, XPathConstants.NODESET);
+            for(int i=0; i < nodes.getLength(); i++) {
+                result.append(nodes.item(i).getNodeValue());
+            }
+            return result.toString();
+        } catch(Exception e) {
+            return "<td style=\"background-color: #ff0000;\">ERROR</td>";
+        }
     }
     
     public String getColor(AbstractBuild<?, ?> target) {
